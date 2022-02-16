d96a3e696adbdaed3dd173c1d48e66187fdf1c31
vasantha <chilukuri.vs@gmail.com>
2022-02-16 11:46:53 +0530

    first commit

---
+++ src/Welcome.js
@@ -0,0 +1,15 @@
+// write code for Welcome component here
+import React from "react";
+import Child from "./Child";
+const Welcome = (props) => {
+  return (
+    <>
+      <h1>Hey {props.name}!</h1>
+      <section>
+       <h2>Welcome to Newton School.</h2>
+      </section>
+    </>
+  );
+};
+
+export default Welcome;
