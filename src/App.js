1a15d5cb2cbbb4f4a72ff7f8d282ebdc1fefb7e3
vassu-somi <96058494+vassu-somi@users.noreply.github.com>
2022-02-23 12:15:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
 import Welcome from "./Welcome";
+//import Child from "./Child";
 
 const App = () => {
   return (
   <>
-  <Welcome name=""/>
-          <h2>Welcome to Newton School</h2>
+   <Welcome/>
           </>
          )
 };
